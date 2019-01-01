dae5e4d74be58eb1b9c472918430c3661c409557
John Carlee <jcarlee@gmail.com>
2019-01-01 15:41:41 -0600

    1/1

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import gspread
+from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('aaaaaaaaaaaaa.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('meta-credecntials.json', scope)
 
 gc = gspread.authorize(credentials)
-aug_sheet = gc.open('THFInformation').worksheet("AugList")
-info_sheet = gc.open('THFInformation').worksheet("Python")
+lib_sheet = gc.open('Game Library').worksheet("Full Library")
+info_sheet = gc.open('Game Library').worksheet("Python")
+
+# Take URL with bs and check MC values
+
