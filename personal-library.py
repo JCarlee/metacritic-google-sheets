0929045acfc8f2cf64784f0f6fe18161f3281710
jcarlee <jcarlee@gmail.com>
2019-01-03 18:04:11 -0600

    fully functional for personal library

--- personal-library.py
+++ personal-library.py
@@ -9,34 +9,43 @@ gc = gspread.authorize(credentials)
 lib_sheet = gc.open('Game Library').worksheet("Library")
 info_sheet = gc.open('Game Library').worksheet("Python")
 
-start_row = info_sheet.acell('A2').value
-end_row = info_sheet.acell('B2').value
+row_start = info_sheet.acell('A2').value
+row_end = info_sheet.acell('B2').value
 base_url = 'https://www.metacritic.com/game/'
 
 
 # Create List of games
 games = []
-game_cells = lib_sheet.range('A' + start_row + ':A' + end_row)
+game_cells = lib_sheet.range('A' + row_start + ':A' + row_end)
 for cell_val in game_cells:
     games.append(cell_val.value.replace(' ', '-').lower())
 
 # Create list of platforms
 plat_list = []
-plat_cells = lib_sheet.range('B' + start_row + ':B' + end_row)
+plat_cells = lib_sheet.range('B' + row_start + ':B' + row_end)
 for cell_val in plat_cells:
     plat_list.append(cell_val.value.replace(' ', '-').lower())
 
+# Re-define steam and uplay as 'pc'
 for n, i in enumerate(plat_list):
     if i == 'steam' or i == 'uplay':
         plat_list[n] = 'pc'
 
+# list of scores taken from spreadsheet
+score_list = lib_sheet.range('C' + row_start + ':C' + row_end)
 
-for game, platform in zip(games, plat_list):
+# main loop to perform essential tasks
+for game, platform, cell in zip(games, plat_list, score_list):
     current_url = base_url + platform + '/' + game
-    print(current_url)
-    req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(req).read()
-    soup = BeautifulSoup(webpage, features="html.parser")
-    name_box = soup.find('span', attrs={'itemprop': 'ratingValue'})
-    box = name_box.text.strip()
-    print(box)
+    try:
+        if cell.value == '':
+            req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
+            webpage = urlopen(req).read()
+            soup = BeautifulSoup(webpage, features="html.parser")
+            name_box = soup.find('span', attrs={'itemprop': 'ratingValue'})
+            box = name_box.text.strip()
+            cell.value = box
+    except:
+        print(current_url + ' not read')
+
+lib_sheet.update_cells(score_list)
