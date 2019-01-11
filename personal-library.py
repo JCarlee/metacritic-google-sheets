2a7fc035eec3dda23a4ea7cbefc2eab3165b6938
John Carlee <jcarlee@gmail.com>
2019-01-11 15:54:37 -0600

    readme build out, more supported platforms

--- personal-library.py
+++ personal-library.py
@@ -10,33 +10,29 @@ lib_sheet = gc.open('Game Library').worksheet("Library")
 gp_sheet = gc.open('Game Library').worksheet("GamePass")
 info_sheet = gc.open('Game Library').worksheet("Python")
 
-lib_row_end = info_sheet.acell('B2').value
-gp_row_end = info_sheet.acell('D2').value
+lib_row_end = info_sheet.acell('A2').value
 base_url = 'https://www.metacritic.com/game/'
 
-
-# Create list of lib_game_list
 lib_game_list = []
 lib_game_cells = lib_sheet.range('A2:A' + lib_row_end)
 for cell_val in lib_game_cells:
     lib_game_list.append(cell_val.value.replace(' ', '-').lower())
 
-# Create list of platforms
-lib_plat_list = []
+lib_platform_list = []
 plat_cells = lib_sheet.range('B:B' + lib_row_end)
 for cell_val in plat_cells:
-    lib_plat_list.append(cell_val.value.replace(' ', '-').lower())
+    lib_platform_list.append(cell_val.value.replace(' ', '-').lower())
 
-# Re-define steam and uplay as 'pc'
-for n, i in enumerate(lib_plat_list):
-    if i == 'steam' or i == 'uplay':
-        lib_plat_list[n] = 'pc'
+pc_platforms = ['steam', 'uplay', 'epic-launcher', 'origin', 'gog']
+for n, i in enumerate(lib_platform_list):
+    if i in pc_platforms:
+        lib_platform_list[n] = 'pc'
 
 # list of scores taken from spreadsheet
 lib_score_list = lib_sheet.range('C2:C' + lib_row_end)
 
 # main loop to perform essential tasks
-for game, platform, cell in zip(lib_game_list, lib_plat_list, lib_score_list):
+for game, platform, cell in zip(lib_game_list, lib_platform_list, lib_score_list):
     current_url = base_url + platform + '/' + game
     try:
         if cell.value == '':
