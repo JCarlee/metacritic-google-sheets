6ead91e8a12f2803633b66c3822550c062bb33cf
John Carlee <jcarlee@gmail.com>
2019-03-31 18:14:01 -0500

    isolated processes into functions

--- game-pass.py
+++ game-pass.py
@@ -6,49 +6,55 @@ from urllib.request import Request, urlopen
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('meta-credentials.json', scope)
 gc = gspread.authorize(credentials)
+
 gp_sheet = gc.open('Game Library').worksheet("GamePass")
 info_sheet = gc.open('Game Library').worksheet("Python")
 
-gp_row_end = info_sheet.acell('B2').value
 base_url = 'https://www.metacritic.com/game/'
-
-gp_game_list = []
+gp_row_end = info_sheet.acell('B2').value
 gp_game_cells = gp_sheet.range('A2:A' + gp_row_end)
-for cell_val in gp_game_cells:
-    gp_game_list.append(cell_val.value.replace(' ', '-').lower())
-
-gp_plat_cells = gp_sheet.range('B2:B' + gp_row_end)
+gp_platform_cells = gp_sheet.range('B2:B' + gp_row_end)
 gp_score_list = gp_sheet.range('C2:C' + gp_row_end)
 
-# Add platform values if don't exist
-for game, plat_cell in zip(gp_game_list, gp_plat_cells):
-    current_url = base_url + 'xbox360/' + game
-    try:
-        if plat_cell.value == '':
-            req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
-            webpage = urlopen(req).read()
-            plat_cell.value = 'Xbox 360'
-    except:
-        plat_cell.value = 'Xbox One'
 
-gp_sheet.update_cells(gp_plat_cells)
+def create_lists():
+    gp_game_list = []
+    gp_plat_list = []
+    for cell_val in gp_game_cells:
+        gp_game_list.append(cell_val.value.replace(' ', '-').lower())
 
-gp_plat_list = []
-for plat_cell_val in gp_plat_cells:
-    gp_plat_list.append(plat_cell_val.value.replace(' ', '-').lower())
+    for g, plat_cell in zip(gp_game_list, gp_platform_cells):
+        c_url = base_url + 'xbox-360/' + g
+        try:
+            if plat_cell.value == '':
+                r = Request(c_url, headers={'User-Agent': 'Mozilla/5.0'})
+                urlopen(r).read()
+                plat_cell.value = 'Xbox 360'
+        except:
+            plat_cell.value = 'Xbox One'
+
+    gp_sheet.update_cells(gp_platform_cells)
+
+    for plat_cell_val in gp_platform_cells:
+        gp_plat_list.append(plat_cell_val.value.replace(' ', '-').lower())
+    return gp_game_list, gp_plat_list
 
 
-# Extract metacritic score
-for game, platform, cell in zip(gp_game_list, gp_plat_list, gp_score_list):
-    current_url = base_url + platform + '/' + game
-    try:
-        if cell.value == '':
-            req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
-            webpage = urlopen(req).read()
-            soup = BeautifulSoup(webpage, features="html.parser")
-            name_box = soup.find('span', attrs={'itemprop': 'ratingValue'})
-            box = name_box.text.strip()
-            cell.value = box
-    except:
-        print(current_url + ' not read')
-gp_sheet.update_cells(gp_score_list)
+def process(i, j):
+    for game, platform, cell in zip(i, j, gp_score_list):
+        current_url = base_url + platform + '/' + game
+        try:
+            if cell.value == '':
+                req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
+                webpage = urlopen(req).read()
+                soup = BeautifulSoup(webpage, features="html.parser")
+                name_box = soup.find('span', attrs={'itemprop': 'ratingValue'})
+                box = name_box.text.strip()
+                cell.value = box
+        except:
+            print(current_url + ' not read')
+    gp_sheet.update_cells(gp_score_list)
+
+
+a, b = create_lists()
+process(a, b)
--- personal-library.py
+++ personal-library.py
@@ -6,43 +6,49 @@ from urllib.request import Request, urlopen
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('meta-credentials.json', scope)
 gc = gspread.authorize(credentials)
+
 lib_sheet = gc.open('Game Library').worksheet("Library")
-gp_sheet = gc.open('Game Library').worksheet("GamePass")
 info_sheet = gc.open('Game Library').worksheet("Python")
 
-lib_row_end = info_sheet.acell('A2').value
 base_url = 'https://www.metacritic.com/game/'
-
-lib_game_list = []
-lib_game_cells = lib_sheet.range('A2:A' + lib_row_end)
-for cell_val in lib_game_cells:
-    lib_game_list.append(cell_val.value.replace(' ', '-').lower())
-
-lib_platform_list = []
-plat_cells = lib_sheet.range('B:B' + lib_row_end)
-for cell_val in plat_cells:
-    lib_platform_list.append(cell_val.value.replace(' ', '-').lower())
-
+lib_row_end = info_sheet.acell('A2').value
+game_cells = lib_sheet.range('A2:A' + lib_row_end)
+platform_cells = lib_sheet.range('B2:B' + lib_row_end)
+score_list = lib_sheet.range('C2:C' + lib_row_end)
 pc_platforms = ['steam', 'uplay', 'epic-launcher', 'origin', 'gog']
-for n, i in enumerate(lib_platform_list):
-    if i in pc_platforms:
-        lib_platform_list[n] = 'pc'
 
-# list of scores taken from spreadsheet
-lib_score_list = lib_sheet.range('C2:C' + lib_row_end)
 
-# main loop to perform essential tasks
-for game, platform, cell in zip(lib_game_list, lib_platform_list, lib_score_list):
-    current_url = base_url + platform + '/' + game
-    try:
-        if cell.value == '':
-            req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
-            webpage = urlopen(req).read()
-            soup = BeautifulSoup(webpage, features="html.parser")
-            name_box = soup.find('span', attrs={'itemprop': 'ratingValue'})
-            box = name_box.text.strip()
-            cell.value = box
-    except:
-        print(current_url + ' not read')
+def create_process_lists():
+    game_list = []
+    platform_list = []
+    for cell_val in game_cells:
+        game_list.append(cell_val.value.replace(' ', '-').lower())
 
-lib_sheet.update_cells(lib_score_list)
+    for cell_val in platform_cells:
+        platform_list.append(cell_val.value.replace(' ', '-').lower())
+
+    for n, i in enumerate(platform_list):
+        if i in pc_platforms:
+            platform_list[n] = 'pc'
+    return game_list, platform_list
+
+
+def process(i, j):
+    for game, platform, cell in zip(i, j, score_list):
+        current_url = base_url + platform + '/' + game
+        try:
+            if cell.value == '':
+                req = Request(current_url, headers={'User-Agent': 'Mozilla/5.0'})
+                webpage = urlopen(req).read()
+                soup = BeautifulSoup(webpage, features="html.parser")
+                name_box = soup.find('span', attrs={'itemprop': 'ratingValue'})
+                box = name_box.text.strip()
+                cell.value = box
+        except:
+            if current_url != 'https://www.metacritic.com/game//':
+                print(current_url + ' not read')
+    lib_sheet.update_cells(score_list)
+
+
+a, b = create_process_lists()
+process(a, b)
